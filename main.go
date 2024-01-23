68857154a722fdce0d98b74e951831176dc730aa
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2024-01-23 14:53:51 +0000

    project initialization

--- main.go
+++ main.go
@@ -1,18 +1,4 @@
 package main
 
-import "database/sql"
 
-type ORM struct {
-	//db *sql.DB
-}
-
-func (o *ORM) InitDB(name string) (db *sql.DB){
-	
-	return
-}
-
-
-func (o *ORM) AutoMigrate(table interface{}) {
-	
-}
 
--- orm.go
+++ orm.go
@@ -1,1 +1,17 @@
 package main
+
+import "database/sql"
+
+type ORM struct {
+	//db *sql.DB
+}
+
+func (o *ORM) InitDB(name string) (db *sql.DB){
+	
+	return
+}
+
+
+func (o *ORM) AutoMigrate(table interface{}) {
+	
+}
