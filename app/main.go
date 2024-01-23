5f516f3c671b13aae5b4e2d5ec5e2377e03a6d9c
bakseck <seck.bakar@ugb.edu.sn>
2024-01-23 16:08:39 +0000

    jwt: done

--- app/main.go
+++ app/main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Bakarseck/orm-go/jwt"
+	"github.com/Bakarseck/orm-go/utils"
+)
+
+type MyPayload struct {
+	Sub  string `json:"sub"`
+	Name string `json:"name"`
+	Iat  int64  `json:"iat"`
+}
+
+func (p *MyPayload) ToJSON() ([]byte, error) {
+	return json.Marshal(p)
+}
+
 func main() {
-	
+	utils.LoadEnv(".env")
+
+	SECRET := os.Getenv("SECRET")
+
+	header := jwt.Header{
+		Alg: "HS256",
+		Typ: "JWT",
+	}
+
+    payload := &MyPayload{
+        Sub:  "1234567890",
+        Name: "Bakar SECK",
+        Iat:  1516239022,
+    }
+
+	jwt, err := jwt.GenerateJWT(header, payload, SECRET)
+
+	if err != nil {
+		log.Println("generate token failed:", err.Error())
+	}
+
+	fmt.Println(jwt)
+
 }
