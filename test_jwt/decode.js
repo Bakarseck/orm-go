3eefce25fdafaa7f8aba5ee22dac62a7b1864da6
bakseck <seck.bakar@ugb.edu.sn>
2024-01-28 17:34:40 +0000

    add: decode.js

--- test_jwt/decode.js
+++ test_jwt/decode.js
@@ -1,32 +1,15 @@
 const token = "eyJhbGciOiJhbGciLCJ0eXAiOiJqd3QifQ.eyJOYW1lIjoiQmFrYXIiLCJJZCI6MTJ9.FZSUE8G6uAilMKM-_viokDdcrH8Lmz92EDLXUVcefns"
 
-const parts = token.split('.');
+const payloadObj = getPayLoad(token);
 
-function base64UrlDecode(str) {
-    str = str.replace(/-/g, '+').replace(/_/g, '/');
-    switch (str.length % 4) {
-        case 0:
-            // Pas de padding nécessaire
-            // La longueur de la chaîne est déjà un multiple de 4
-            break;
-        case 2:
-            // Ajouter deux caractères de padding
-            // La chaîne a besoin de 2 caractères supplémentaires pour atteindre un multiple de 4
-            str += '==';
-            break;
-        case 3:
-            // Ajouter un caractère de padding
-            // La chaîne a besoin de 1 caractère supplémentaire pour atteindre un multiple de 4
-            str += '=';
-            break;
-        default:
-            // Si aucun des cas ci-dessus n'est vrai, alors la chaîne est invalide pour du Base64
-            throw 'Chaîne Base64URL invalide';
-    }
-    return atob(str);
+function getPayLoad(token) {
+    const parts = token.split('.');
+    const payload = parts[1].replace(/-/g, '+').replace(/_/g, '/');
+    const paddedPayload = payload.padEnd(payload.length + (4 - payload.length % 4) % 4, '=');
+    const decodedPayload = atob(paddedPayload);
+    const payloadObj = JSON.parse(decodedPayload);
+    return payloadObj;
 }
 
-const payload = base64UrlDecode(parts[1]);
-const payloadObj = JSON.parse(payload);
-
 console.log(payloadObj);
+
