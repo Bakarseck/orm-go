085dba1b694715f45601ee235cc7bb25a5e525b8
bakseck <seck.bakar@ugb.edu.sn>
2024-01-28 15:20:29 +0000

    add: comment for decode.js

--- test_jwt/decode.js
+++ test_jwt/decode.js
@@ -6,14 +6,21 @@ function base64UrlDecode(str) {
     str = str.replace(/-/g, '+').replace(/_/g, '/');
     switch (str.length % 4) {
         case 0:
+            // Pas de padding nécessaire
+            // La longueur de la chaîne est déjà un multiple de 4
             break;
         case 2:
+            // Ajouter deux caractères de padding
+            // La chaîne a besoin de 2 caractères supplémentaires pour atteindre un multiple de 4
             str += '==';
             break;
         case 3:
+            // Ajouter un caractère de padding
+            // La chaîne a besoin de 1 caractère supplémentaire pour atteindre un multiple de 4
             str += '=';
             break;
         default:
+            // Si aucun des cas ci-dessus n'est vrai, alors la chaîne est invalide pour du Base64
             throw 'Chaîne Base64URL invalide';
     }
     return atob(str);
