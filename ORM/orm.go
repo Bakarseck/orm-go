30eae92e9a0dece6dcac86e14a8e2e184c55c65b
mouhamsylla <syllamouhamed99@gmail.com>
2024-05-18 19:08:20 +0000

    fix: InitDB(name string, path string) with now two parameters: db filename and db path

--- ORM/orm.go
+++ ORM/orm.go
@@ -7,32 +7,42 @@ import (
 	"os"
 	"reflect"
 	"strings"
+	"time"
 )
+
+var PATH string
 
 // The `InitDB` function is responsible for initializing the database connection and creating the
 // necessary files and directories for database migration.
-func (o *ORM) InitDB(name string) {
-	_, err := os.Stat(name)
+func (o *ORM) InitDB(name string, path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0775)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
+	_, err := os.Stat(path + name)
 	if os.IsNotExist(err) {
-		file, err := os.Create(name)
+		file, err := os.Create(path + name)
 		if err != nil {
 			log.Fatal(err)
 		}
 		file.Close()
 	}
 
-	if _, err := os.Stat("migrates"); os.IsNotExist(err) {
-		err := os.Mkdir("migrates", 0755)
+	if _, err := os.Stat(path + "migrates"); os.IsNotExist(err) {
+		err := os.Mkdir(path + "migrates", 0755)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	o.Db, err = sql.Open("sqlite3", name)
+	o.Db, err = sql.Open("sqlite3", path+name)
 	if err != nil {
 		log.Fatal(err)
 	}
+	PATH = path
 }
 
 // The CreateTable function creates a SQL table with the given name and fields.
@@ -40,7 +50,7 @@ func CreateTable(name string, fields ...*Field) string {
 	sqlTable := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (\n", name)
 	var all []string
 	for _, field := range fields {
-		all = append(all, "\t"+TableField(field))
+		all = append(all, "\t" + TableField(field))
 	}
 	sqlTable += strings.Join(all, ",\n") + "\n)"
 	return sqlTable
@@ -65,32 +75,17 @@ func (o *ORM) AutoMigrate(tables ...interface{}) {
 			panic(err)
 		}
 
-		upFileName := fmt.Sprintf("create-table-%s.up.sql", v.Name())
-		downFileName := fmt.Sprintf("drop-table-%s.down.sql", v.Name())
+		currentTime := time.Now()
+		fileName := fmt.Sprintf("%smigrates/%s-create-table-%s.sql", PATH, currentTime.Format("2006-01-02-15-04-05"), v.Name())
+		file, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
 
-		if _, err := os.Stat(upFileName); os.IsNotExist(err) {
-			file, err := os.Create(upFileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-
-			_, err = file.WriteString(createTableSQL)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			downFile, err := os.Create(downFileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer downFile.Close()
-
-			dropTableSQL := fmt.Sprintf("DROP TABLE IF EXISTS %s;", v.Name())
-			_, err = downFile.WriteString(dropTableSQL)
-			if err != nil {
-				log.Fatal(err)
-			}
+		_, err = file.WriteString(createTableSQL)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
